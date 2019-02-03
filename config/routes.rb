443b6967cc73e32b8e59044f8d56cde674e2629f
Patricio Andrés Peña Muñoz <patriciopenamunoz@gmail.com>
2019-02-03 00:04:36 -0300

    Allow to access to subcategory through category

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
   resources :recipes
   resources :foods
   resources :sources
-  resources :subcategories
-  resources :categories
+  resources :subcategories, only: [:new, :create, :edit, :update, :destroy]
+  resources :categories do
+    resources :subcategories, only: [:index]
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
