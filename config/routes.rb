ad8dbaf60401770cc510670a714031ac8531f3c7
juanraba <jrabanillog@gmail.com>
2019-02-03 05:38:18 -0300

    tablefilter

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   get 'pages/index'
+  get 'pages/tablefilter'
   devise_for :users
   root to: "pages#index"
   resources :compounds
